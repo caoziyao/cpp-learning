6d5ac54ffffebcc1e56b4d140fc5b841f9bcc36a
cczy <wyzycao@gmail.com>
2018-08-17 00:24:00 +0800

    update gitignore

---
+++ lambda1/lambda1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lambda1
+//
+//  Created by working on 2018/8/17.
+//  Copyright © 2018年 working. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
